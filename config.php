6cad95f2d6ce033042cbfc817b4163b9f6e8f169
cmsllince <cmsllince@gmail.com>
2014-09-07 00:39:27 +0300

    Update config.php

--- config.php
+++ config.php
@@ -8,14 +8,6 @@ $twitter = array(
     'access_token_secret' =>    '10g7MCboaRr5YWC57X3yBOyv9HFHXMtsJskcTlS93DxKB'
 );
 
-$instagram = array(
-    'client_id' =>      'afffed81d9704aeeb9d2c3949352c716',
-    'client_secret' =>  '0f7d7c8c54e04e90849425148c14b4f2',
-    'website_url' =>    'http://127.0.0.1/A-social-Nairobi-master/',
-    'redirect_uri' =>   'http://127.0.0.1/A-social-Nairobi-master/',
-    'access_token' =>   'fb985abc1ff94cd3bf43433b1aca6b4b'
-);
-
 $db = array(
     'host' =>       '127.0.0.1',
     'user' =>       'root',
