79cf0677b00629251b0dd01bb136e2fc09d68e53
Collins Masero <cmsllince@gmail.com>
2020-07-30 21:24:12 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 date_default_timezone_set('Africa/Nairobi');
 
 $twitter = array(
-    'consumer_key' =>           'wcftnhEIdIlFIuxd2zXqXg',
-    'consumer_secret' =>        'KXotF8Gd7SLhzsDjSeFzeNpNlbbQQ2MJNDHfsSjBq5o',
-    'access_token' =>           '398719043-bknZ2vpZMb1ekpCFykL9o5yLEsr187eQMpNtF9QJ',
-    'access_token_secret' =>    '10g7MCboaRr5YWC57X3yBOyv9HFHXMtsJskcTlS93DxKB'
+    'consumer_key' =>           '',
+    'consumer_secret' =>        '',
+    'access_token' =>           '',
+    'access_token_secret' =>    ''
 );
 
 $db = array(
